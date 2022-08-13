86469fb9773b31fa3b451afb6302c72f2f5fbc1b
sheready <csstlw@gmail.com>
2022-08-13 13:17:11 +0300

    create components folder

---
+++ dashboard/src/components/AddUser.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const AddUser = () => {
+    return (
+        <div className='container'>
+            <div className='row'>
+                <div className='col-md-7'>
+                    <h1>Add User Form</h1>
+                </div>
+            </div>
+        </div>
+
+
+    )
+}
+export default AddUser
